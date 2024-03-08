45ab03597233a68557af8d65829b51fa0399470c
Ulrik Pedersen <ulriktpedersen@nttdata.com>
2024-03-08 14:33:30 +0100

    functions

---
+++ functions/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    println!("Hello, world!");
+
+    another_function(5);
+}
+
+fn another_function(x: i32) {
+    println!("Another, function with the parameter {x}");
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char) {
+    println!("The measurement is: {value}{unit_label}");
+}

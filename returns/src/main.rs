102a3287557061aa6f975725d9bb2c87f8f3aac5
Ulrik Pedersen <ulriktpedersen@nttdata.com>
2024-03-12 14:20:57 +0100

    fixes

--- returns/src/main.rs
+++ returns/src/main.rs
@@ -1,5 +1,33 @@
 fn main() {
-    let x = 1;
 
-    
+    let s1 = gives_ownership();
+
+    let s2 = String::from("Hello");
+
+    let s3 = takes_and_gives_back(s2.clone());
+
+    println!("{s1} {s2} {s3}");
+
+    let s1 = String::from("Hello");
+
+    let (s2, len) = calculate_len(s1);
+
+    println!("The length of {s2} is {len}")
+
+}
+
+fn gives_ownership() -> String {
+    let some_string = String::from("It's yours!");
+
+    some_string
+}
+
+fn takes_and_gives_back(a_string: String) -> String {
+    a_string
+}
+
+fn calculate_len(s: String) -> (String, usize) {
+    let length = s.len();
+
+    (s, length)
 }
